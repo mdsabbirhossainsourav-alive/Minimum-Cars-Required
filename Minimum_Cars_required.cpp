c044673061c291af21a6847a77a2d9798bc84fc4
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2025-12-07 13:02:50 +0600

    Add files via upload

---
+++ Minimum_Cars_required.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++) 
+    {
+        int n;
+        cin >> n;
+        int cars = (n+3)/4;
+        cout << cars << endl;
+    }
+    return 0;
+}
